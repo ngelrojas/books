2858bad7bfb4f59e9ff7ec6c312bc06a3d8e6491
ngel rojas <aomarrojasp@gmail.com>
2023-03-28 19:53:14 -0300

    delete: deleted comments about edit, delete, update methods

--- src/App.js
+++ src/App.js
@@ -17,31 +17,6 @@ const App = () => {
       {id: Math.round(Math.random() * 9999), title}
     ];
     setBook(updateBooks);
-    // 1 adding new element to array
-    // const updateColor = [...book, title];
-    // setBook(updateColor);
-
-    // 2 adding new element to array using position of element
-    // const updateColor =[
-    //   ...book.slice(0, index),
-    //   title,
-    //   ...book.slice(index)
-    // ]
-    // setBook(updateColor);
-
-    // 3 removing element from array 
-    // const updateColor = book.filter((book, i) => i !== index);
-    // setBook(updateColor);
-
-    // 4 update book
-    // the title my be title: title, but its the same name and using
-    // javascript tricks we can use just one name in line 32
-    // const updateBooksID = (id, title) => {
-    //   const updateBook = books.map((book) => (book.id === id ? {..book, title} : book}));
-    //   setBooks(updateBook);
-    // }
-
-    // 
   }
 
   const deleteBookId = (id) => {
