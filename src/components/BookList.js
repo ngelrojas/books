8787736ec1110268561b4e05a74776ffbccb53e6
ngel rojas <aomarrojasp@gmail.com>
2023-03-29 20:27:37 -0300

    add: adding context in the project, using useContext, and CreateContext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,17 @@
+import { useContext } from 'react';
 import BookShow from './BookShow';
+import BooksContext from '../context/books';
 
 const BookList = ({books, onDelete, onEdit}) => {
+    const value = useContext(BooksContext);
+    
     const renderBooks = books.map((book) => {
         return <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete} />
     })
 
     return (
         <div className="book-list">
+            {value}
         {renderBooks}
         </div>
     );
